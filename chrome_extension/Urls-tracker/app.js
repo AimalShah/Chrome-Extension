d6935d778f37c0dc091075bcf6969212d0dbf731
Aimal Shah <aimalshah62@gmail.com>
2023-02-21 12:35:29 +0500

    version updated

--- chrome_extension/Urls-tracker/app.js
+++ chrome_extension/Urls-tracker/app.js
@@ -1,27 +1,56 @@
 let myUrls = []
-const input = document.querySelector("#input-el");
-const inputBtn = document.querySelector("#input-btn");
-const ulEl = document.querySelector("#ul-el");
+const input = document.querySelector("#input-el")
+const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
+console.log(tabBtn)
 
 
-inputBtn.addEventListener('click', () => {
-    
-        myUrls.push(input.value);
-        console.log(myUrls);
-        renderUrls();          
-        input.value = "";
 
-});
-function renderUrls(){
-
-    let listItems = "";
-    for(let i = 0; i < myUrls.length; i++){
-        console.log(myUrls[i]);
-        listItems +=   `<li>
-                        <a target='_blank' href="${myUrls[i]}">
-                        ${myUrls[i]}
-                        </a>
-                        </li>`;
-    }
-    ulEl.innerHTML = listItems;
+let urlsFromLocalStorage = JSON.parse(localStorage.getItem("myUrls")) 
+if(urlsFromLocalStorage){
+  myUrls = urlsFromLocalStorage;
+  render(myUrls)
 }
+
+
+
+
+tabBtn.addEventListener('click', () => {
+  chrome.tabs.query({active : true , currentWindow : true}, (tabs) =>{
+    myUrls.push(tabs[0].url)
+    localStorage.setItem("myUrls" , JSON.stringify(myUrls))
+    render(myUrls)
+  })
+  })
+
+function render(Urls) {
+  let listItems = ""
+  for (let i = 0; i < Urls.length; i++) {
+    console.log(myUrls[i])
+    listItems += `<li>
+                        <a target='_blank' href="${myUrls[i]}">
+                        ${Urls[i]}
+                        </a>
+                 </li>`
+  }
+  ulEl.innerHTML = listItems
+}
+
+
+deleteBtn.addEventListener("click" , () =>{
+  alert('If you want to delete all your data press "OK"')
+  localStorage.clear()
+  myUrls = []
+  render(myUrls)
+
+})
+
+inputBtn.addEventListener("click", () => {
+  myUrls.push(input.value)
+  console.log(myUrls)
+  render(myUrls)
+  input.value = ""
+  localStorage.setItem("myUrls", JSON.stringify(myUrls))
+})
